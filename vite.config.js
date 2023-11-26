0b7a2f84a739d033ddf55b9cd675554ae3511ee3
Jens Küsters <jens@kuesters.net>
2023-11-26 16:24:46 +0100

    ⬆️Updated to Vite 5

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { resolve } from 'path'
-import kirby from "vite-plugin-kirby";
-import mkcert from "vite-plugin-mkcert";
+import { resolve } from 'path';
+import kirby from 'vite-plugin-kirby';
+import mkcert from 'vite-plugin-mkcert';
 
 export default ({ mode }) => ({
   root: 'src',
@@ -9,18 +9,12 @@ export default ({ mode }) => ({
   build: {
     outDir: resolve(process.cwd(), 'public/dist'),
     emptyOutDir: true,
-    rollupOptions: { input: resolve(process.cwd(), 'src/index.js') }
+    rollupOptions: { input: [resolve(process.cwd(), 'src/index.js'), resolve(process.cwd(), 'src/index.css')] },
   },
 
-  plugins: [
-    kirby([
-      '../content/**/*',
-      '../site/(templates|snippets|controllers|models)/**/*.php'
-    ]),
-    mkcert()
-  ],
+  plugins: [kirby(['../content/**/*', '../site/(templates|snippets|controllers|models)/**/*.php']), mkcert()],
   server: {
     https: true,
-    host: 'localhost'
-  }
-})
+    host: 'localhost',
+  },
+});
